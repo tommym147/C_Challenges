527a8982859bbec5d32469de8f2a9091e4b81e8f
Tommy <tommym147@gmail.com>
2018-01-17 13:41:46 -0800

    added Recursion folder and PrintArray challenge

---
+++ HowToProgram/Recursion/PrintArray/PrintArray.c
@@ -0,0 +1,27 @@
+// Thomas Mejia
+// 1/17/2018
+// Write a recursive function that prints an array and returns nothing.
+
+#include <stdlib.h>
+#include <stdio.h>
+#define SIZE 10
+
+void printArray(int array [], int size) { // function that prints each element in array recursively
+	size--;
+	if (size == 0) { // base case
+		printf("%d ",array[size]);
+		return;
+	}
+	printArray(array, size);
+	printf("%d ",array[size]);	
+}
+
+int main(void) {
+	int array [SIZE] = {5, 33, 21, 99, 75, 87, 90, 34, 23, 98};
+
+	puts("Content of array: ");
+	printArray(array, SIZE);
+	puts("");
+
+	return 0;
+}
